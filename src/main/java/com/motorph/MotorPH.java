f84a891ac891b5cc99d5c66ee5055b9ebf491a59
maryyygb <marygbasilio8@gmail.com>
2025-02-25 23:38:55 +0800

    First Commit

---
+++ src/main/java/com/motorph/MotorPH.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motorph;
+
+/**
+ *
+ * @author Mary Grace Basilio
+ */
+public class MotorPH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
---
+++ src/test/java/SheetsQuickstart.java
@@ -0,0 +1,109 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class SheetsQuickstart {
+  private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
+  private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+  private static final String TOKENS_DIRECTORY_PATH = "tokens/path";
+
+  /**
+   * Global instance of the scopes required by this quickstart.
+   * If modifying these scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES =
+      Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+      throws IOException {
+    // Load client secrets.
+    InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    if (in == null) {
+      throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+    }
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+        .setAccessType("offline")
+        .build();
+    LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+    return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+  }
+
+  /**
+   * Prints the names and majors of students in a sample spreadsheet:
+   * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+     * @param args
+     * @throws java.io.IOException
+     * @throws java.security.GeneralSecurityException
+   */
+  public static void main(String... args) throws IOException, GeneralSecurityException {
+    // Build a new authorized API client service.
+    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//    final String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+//    final String spreadsheetId = "1bG5fzznjJXTZfmhjs6exC7-eJsDgLmRX3HtpimaxW9o";
+
+//Employee Details
+      final String spreadsheetId = "1jd1kNFFZSx3hBqVgwrP7AATWPX9YQHTfKjpQQxyO1zA";
+
+//Employee Details
+    final String range = "Employee Details!A1:S";
+    Sheets service =
+        new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            .setApplicationName(APPLICATION_NAME)
+            .build();
+    ValueRange response = service.spreadsheets().values()
+        .get(spreadsheetId, range)
+        .execute();
+    List<List<Object>> values = response.getValues();
+    
+    if (values == null || values.isEmpty()) {
+      System.out.println("No data found.");
+    } else {
+
+      for (List row : values) {
+          
+          String employee_id = (String) row.get(0);
+          
+            System.out.println(
+                                employee_id + ", " + row.get(2) + " " + row.get(1) + ", " + 
+                                row.get(11) + ", " + row.get(10) + ", " + row.get(13) + ", " + 
+                                row.get(17) + ", " + row.get(18)
+                              );
+
+          
+      }
+    }
+  }
+}
